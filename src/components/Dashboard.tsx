548a298e48f367d2f389f4a9f420cae676b52065
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-08-29 01:15:41 +0530

    added data

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   IconArrowLeft,
   IconBrandTabler,
@@ -110,12 +109,11 @@ export const LogoIcon = () => {
   );
 };
 
-// Dummy dashboard component with content
 export const Dashboard = () => {
   return (
     <div className="flex flex-1">
       <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
+        {/* <div className="flex gap-2">
           {[...new Array(4)].map((i) => (
             <div
               key={"first-array" + i}
@@ -130,7 +128,7 @@ export const Dashboard = () => {
               className="h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
             ></div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
